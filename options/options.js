07aba12e5367821b4895ac96fd661a2c804c534d
Crowdbotics <team@crowdbotics.com>
2021-08-05 17:43:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "black_union_29526Identifier",
+  url: "https://black_union_29526Identifier.botics.co",
+  api: "https://black_union_29526Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
